0a5e4cde0977c584f1fa0786e36f1e4ce3aeb72c
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2021-10-09 19:22:22 +0200

    Update

--- java/code/src/main/java/org/nbicocchi/swing/puzzle/Puzzle.java
+++ java/code/src/main/java/org/nbicocchi/swing/puzzle/Puzzle.java
@@ -9,7 +9,6 @@ import java.awt.image.BufferedImage;
 import java.awt.image.CropImageFilter;
 import java.awt.image.FilteredImageSource;
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
