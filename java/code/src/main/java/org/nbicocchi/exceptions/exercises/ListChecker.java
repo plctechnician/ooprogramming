ac5784debcfb15cbbda628c250aacc55aceac895
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2021-10-13 15:30:16 +0200

    Update

--- java/code/src/main/java/org/nbicocchi/exceptions/exercises/ListChecker.java
+++ java/code/src/main/java/org/nbicocchi/exceptions/exercises/ListChecker.java
@@ -12,6 +12,16 @@ import java.util.List;
  * @author Nicola Bicocchi
  */
 public class ListChecker {
+    public void check(List<String> strings) {
+        StringChecker checker = new StringChecker();
+        for (Iterator<String> i = strings.iterator(); i.hasNext(); ) {
+            try {
+                checker.check(i.next());
+            } catch (ParseException e) {
+                i.remove();
+            }
+        }
+    }
 
     public static void main(String[] args) {
         List<String> l = new ArrayList<>();
@@ -20,21 +30,9 @@ public class ListChecker {
         l.add("r2r6t8f0d00");
         l.add("z1z0t8f1d7d");
 
-        StringChecker checker = new StringChecker();
-        for (Iterator<String> i = l.iterator(); i.hasNext(); ) {
-            String s = i.next();
-            try {
-                System.out.printf("Checking %s ", s);
-                checker.check(s);
-                System.out.println("[PASS]");
-            } catch (ParseException e) {
-                i.remove();
-                System.out.println("[FAIL]");
-            }
-        }
+        ListChecker lc = new ListChecker();
+        lc.check(l);
 
-        for (String s : l) {
-            System.out.println(s);
-        }
+        System.out.println(l);
     }
 }
