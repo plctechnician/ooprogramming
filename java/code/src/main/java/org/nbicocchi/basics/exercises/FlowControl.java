035d9d74ff41b12a2b6c5155a3a6a418aaf1c90b
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-01-11 16:07:34 +0100

    Update

--- java/code/src/main/java/org/nbicocchi/basics/exercises/FlowControl.java
+++ java/code/src/main/java/org/nbicocchi/basics/exercises/FlowControl.java
@@ -7,6 +7,18 @@ package org.nbicocchi.basics.exercises;
  * Code -> Folding -> Expand Doc Comments
  */
 public class FlowControl {
+
+    /**
+     * Write a function printing all integer numbers divisible by 7 comprised between 0 and 100
+     */
+    static void divisibleBy7() {
+        for (int i = 0; i < 100; i++) {
+            if (i % 7 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+
     /**
      * Write a function returning the sum of the first 100 positive integer numbers
      */
