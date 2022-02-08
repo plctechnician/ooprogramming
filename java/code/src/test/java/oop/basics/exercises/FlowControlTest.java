b66f120d803da20552241bf6ac6dbc67f48c903d
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-02-08 14:31:10 +0100

    update

--- java/code/src/test/java/oop/basics/exercises/FlowControlTest.java
+++ java/code/src/test/java/oop/basics/exercises/FlowControlTest.java
@@ -7,11 +7,13 @@ import static org.junit.Assert.*;
 public class FlowControlTest {
     @Test
     public void sumFirstHundredNumbers() {
+
         assertEquals(5050, FlowControl.sumFirstHundredNumbers());
     }
 
     @Test
     public void sumFirstNNumbers() {
+
         assertEquals(5050, FlowControl.sumFirstNNumbers(100));
     }
 }
