2d7e42ce81fe0529896343be0eb8a92c3beb95a6
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-02-11 16:11:36 +0100

    update

--- java/code/src/main/java/oop/swing/CelsiusConverterFullUIDesigner.java
+++ java/code/src/main/java/oop/swing/CelsiusConverterFullUIDesigner.java
@@ -2,8 +2,6 @@ package oop.swing;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class CelsiusConverterFullUIDesigner extends JFrame {
     private JPanel mainPanel;
@@ -12,36 +10,30 @@ public class CelsiusConverterFullUIDesigner extends JFrame {
     private JButton btCelsius;
     private JButton btFahrenheit;
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(CelsiusConverterFullUIDesigner::new);
-    }
-
     public CelsiusConverterFullUIDesigner() throws HeadlessException {
         setContentPane(mainPanel);
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        btCelsius.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                double tempFahrenheit = Double.parseDouble(tfCelsius.getText()) * 1.8 + 32;
-                tfFahrenheit.setText(Double.toString(tempFahrenheit));
-                if (tempFahrenheit < 32) {
-                    JOptionPane.showMessageDialog(null, "Water freezes here!", "Temperature Warning", JOptionPane.WARNING_MESSAGE);
-                }
+        btCelsius.addActionListener(e -> {
+            double tempFahrenheit = Double.parseDouble(tfCelsius.getText()) * 1.8 + 32;
+            tfFahrenheit.setText(Double.toString(tempFahrenheit));
+            if (tempFahrenheit < 32) {
+                JOptionPane.showMessageDialog(null, "Water freezes here!", "Temperature Warning", JOptionPane.WARNING_MESSAGE);
             }
         });
-        btFahrenheit.addActionListener(new ActionListener() {
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                double tempCelsius = (Double.parseDouble(tfFahrenheit.getText()) - 32) * 0.555;
-                tfCelsius.setText(Double.toString(tempCelsius));
-                if (tempCelsius < 0) {
-                    JOptionPane.showMessageDialog(null, "Water freezes here!", "Temperature Warning", JOptionPane.WARNING_MESSAGE);
-                }
+        btFahrenheit.addActionListener(e -> {
+            double tempCelsius = (Double.parseDouble(tfFahrenheit.getText()) - 32) * 0.555;
+            tfCelsius.setText(Double.toString(tempCelsius));
+            if (tempCelsius < 0) {
+                JOptionPane.showMessageDialog(null, "Water freezes here!", "Temperature Warning", JOptionPane.WARNING_MESSAGE);
             }
         });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(CelsiusConverterFullUIDesigner::new);
     }
 }
