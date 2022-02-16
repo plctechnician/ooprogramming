58e310faa592109c835bafda34b70eb5c336c4c6
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-02-16 23:06:46 +0100

    update

--- java/code/src/main/java/oop/objectoriented/exercises/reverse/TestApp.java
+++ java/code/src/main/java/oop/objectoriented/exercises/reverse/TestApp.java
@@ -4,6 +4,9 @@ package oop.objectoriented.exercises.reverse;
  * Use the Reverser Interface to provide the following main() with
  * three different (but interchangeable) objects for reversing a String.
  * The reverse() method accepts a String and returns the same String, but inverted.
+ * EngineSlow uses String.charAt() and String concatenation
+ * EngineFast uses StringBuilder() instead of String concatenation
+ * EngineBest uses StringBuilder() only
  */
 
 public class TestApp {
@@ -12,7 +15,7 @@ public class TestApp {
         //Reverser r = new EngineFast();
         //Reverser r = new EngineSlow();
 
-        //Using lambda expressions
+        //One line version with lambda expressions
         //Reverser r = (String s) -> new StringBuilder(s).reverse().toString();
 
         System.out.println(r.reverse("Hello World!"));
