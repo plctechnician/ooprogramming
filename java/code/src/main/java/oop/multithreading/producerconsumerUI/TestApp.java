3db7c6e71cd4b0a42b434457b5b51190afe366f0
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-04-14 16:49:41 +0200

    update

--- java/code/src/main/java/oop/multithreading/producerconsumerUI/TestApp.java
+++
@@ -1,9 +0,0 @@
-package oop.multithreading.producerconsumerUI;
-
-import java.awt.*;
-
-public class TestApp {
-    public static void main(String[] args) {
-        EventQueue.invokeLater(ProducerConsumerGUI::new);
-    }
-}
