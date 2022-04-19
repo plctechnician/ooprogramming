f27d988b7972bd29e2e822e461f98fca23536e6d
Nicola Bicocchi <nicola.bicocchi@gmail.com>
2022-04-19 21:30:33 +0200

    update

--- java/code/src/main/java/oop/swing/planemanager/TestApp.java
+++
@@ -1,9 +0,0 @@
-package oop.swing.planemanager;
-
-import javax.swing.*;
-
-public class TestApp {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(PlanesUIDesigner::new);
-    }
-}
